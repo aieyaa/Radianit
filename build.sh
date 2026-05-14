1441989b134eb9cee16ba0f363b845a7af13184c
aieyaa <elea.ya.pro@gmail.com>
2026-05-14 18:16:08 +0200

    feat: add database seeding and automated superuser creation to build script

--- build.sh
+++ build.sh
@@ -1,29 +1,18 @@
 #!/usr/bin/env bash
-
-
-
 # exit on error
-
-
-
 set -o errexit
-
-
-
-
-
-
 
 pip install -r requirements.txt
 
-
-
-
-
-
-
 python manage.py collectstatic --no-input
-
-
-
 python manage.py migrate
+
+# Peupler la base de données
+echo "Peuplement de la base de données..."
+python seed_data.py
+
+# Créer un superutilisateur
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+    echo "Tentative de création du superutilisateur ($DJANGO_SUPERUSER_USERNAME)..."
+    python manage.py createsuperuser --no-input || echo "Le superutilisateur existe déjà ou une erreur est survenue."
+fi
